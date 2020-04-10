e2fc486f27cab3b75e0a150f9acf06680097067f
Michael Wehar <mwehar@buffalo.edu>
2020-04-10 04:25:01 -0400

    Added explanation

--- python/one_rect.py
+++ python/one_rect.py
@@ -1,6 +1,27 @@
 # Code written by Joseph Swernofsky
-# Note: You may need to transpose the matrix if m > n
-# in order to have runtime O(m*n).
+#
+# This approach is slightly different from the Java approach
+# that is described in the Readme.
+#
+# Key details:
+# (a) We go through the matrix row by row.
+# (b) When we encounter a one in the matrix we add its row
+# index to the set row_sets[col] where col is the current
+# column index.
+# (c) For each row, we build up a set union of row indexes
+# where there is a one in the same column as a one in the
+# current row.
+# (d) If we ever have a non-empty intersection between
+# row_sets[col] and union, then we've found a 2x2 subrect.
+# This is because we have two ones in the current row that
+# both have ones above them at a matching row index.
+# (e) The runtime is m^2 + m*n. We have m*n because we visit
+# every entry in the matrix. We have m^2 because for each
+# row we add at most m row indexes to union or else we would
+# have found a duplicate meaning that there is a 2x2 subrect.
+#
+# Note: You need to transpose the matrix if m > n so that the
+# runtime is O(m*n).
 
 m,n = map(int, input().split())
 matrix = [list(map(int, input())) for _ in range(m)]
