b7f3aadac7ca04027b6230094b9a78fb8184330f
Ari Liloia <aliloia1@swarthmore.edu>
2020-05-26 13:21:17 -0400

    o

--- python/primary_implementation.py
+++ python/primary_implementation.py
@@ -8,25 +8,48 @@ def createEmptyArray(n):
 def createEmptyMatrix(m, n):
     return [createEmptyArray(n) for _ in range(m)]
 
-m = 5
-n = 5
+def importMatrix(filename):
+    #open files with the following convention:
+    #first line: #rows (m), #columns (n)
+    #subsequent lines: m by n matrix of ones and zeros
 
-emptyMatrix = createEmptyMatrix(5, 5)
-testMatrix = emptyMatrix
-testMatrix[0][0] = True
-testMatrix[0][4] = True
-testMatrix[4][4] = True
-testMatrix[4][0] = True
+    #read the first line of the file, containing m, n
+    c_file = open(filename, "r")
+    #convert m, n into integers
+    mRowsString, mColsString = c_file.readline().split()
+    mRows = int(mRowsString)
+    mCols = int(mColsString)
+    #create an empty 1d array with m rows
+    returnMatrix = createEmptyArray(mRows)
+    rowIndex = 0
+    #for each subsequent line in the file (each line of the matrix)
+    for line in c_file:
+        #split each row into an array of integer ones and zeros
+        matrixRow = [ int(x) for x in line.split() ]
+        #if the number of buckets in one row is not the number of columns
+        #implicated at the top of the file:
+        if(len(matrixRow) != mCols):
+            #throw an error and return nothing
+            print("Error: incorrect number of columns in row " + str(rowIndex))
+            return
+        returnMatrix[rowIndex] = matrixRow
+        #increment the row 'cursor'
+        rowIndex = rowIndex + 1
+    #if the number of rows accessed so far is not the number of rows
+    #implicated at the top of the file:
+    if(rowIndex != mRows):
+        #throw an error and return nothing
+        print("Error: too few rows")
+        return
+    c_file.close()
+    #close matrix file
+    return mRows, mCols, returnMatrix
+    #If there are too many rows, an error message will be displayed in the terminal
+    #because nonexistent buckets of the matrix will be accessed.
 
-print(testMatrix)
-# bijection
-# 4 cols
-# (0,4): (0 * 4) + 4 = 4
-# (1,0): (1 * 4) + 0 = 4
-# this is prob why we start at an index 1 greater
-# but why is it working w this code
+m, n, testMatrix = importMatrix('test_matrices/testMatrix1')
 
-def rectExists(matrix, m, n):
+def rectExists(m, n, matrix):
     # This array will assign data to each pair of columns
     columnPairs = createEmptyArray(n * n)
     # Traverse through the matrix row by row
@@ -52,4 +75,4 @@ def rectExists(matrix, m, n):
                     columnPairs[(firstElement * n) + nextElement] = True
     return False
 
-print(rectExists(testMatrix, m, n))
+print(rectExists(m, n, testMatrix))
