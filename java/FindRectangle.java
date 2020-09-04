fb59e80d9c6b2d0d1137a9df38274648eee3c79e
Michael Wehar <dff72324@gmail.com>
2020-09-04 13:26:46 -0400

    Small improvement to Java code

--- java/FindRectangle.java
+++ java/FindRectangle.java
@@ -35,18 +35,18 @@ public class FindRectangle {
         Map<List<Integer>, Integer> pairs = new HashMap<>();
         for (int i = 0; i < rows; i++) {
             List<Integer> values = adjList.get(i);
-            for (int j = 0; j < values.size(); j++) {
+            for (int j = 0; j < values.size() - 1; j++) {
                 for (int k = j + 1; k < values.size(); k++) {
                     List<Integer> pair = Arrays.asList(new Integer[] { values.get(j), values.get(k) });
                     if (pairs.containsKey(pair)) {
                         // ##### START DEBUGGING #####
-                        if (transposed) {
-                            System.out.println("ROWS: " + values.get(j) + ", " + values.get(k));
-                            System.out.println("COLS: " + pairs.get(pair) + ", " + i);
-                        } else {
-                            System.out.println("ROWS: " + pairs.get(pair) + ", " + i);
-                            System.out.println("COLS: " + values.get(j) + ", " + values.get(k));
-                        }
+                        // if (transposed) {
+                        //     System.out.println("ROWS: " + values.get(j) + ", " + values.get(k));
+                        //     System.out.println("COLS: " + pairs.get(pair) + ", " + i);
+                        // } else {
+                        //     System.out.println("ROWS: " + pairs.get(pair) + ", " + i);
+                        //     System.out.println("COLS: " + values.get(j) + ", " + values.get(k));
+                        // }
                         // ##### END DEBUGGING #####
                         return true;
                     } else {
@@ -63,9 +63,9 @@ public class FindRectangle {
     public static boolean naiveSearch(boolean[][] matrix) {
         int rows = MatrixLibrary.getRows(matrix);
         int cols = MatrixLibrary.getCols(matrix);
-        for (int row1 = 0; row1 < rows; row1++) {
+        for (int row1 = 0; row1 < rows - 1; row1++) {
             for (int row2 = row1 + 1; row2 < rows; row2++) {
-                for (int col1 = 0; col1 < cols; col1++) {
+                for (int col1 = 0; col1 < cols - 1; col1++) {
                     for (int col2 = col1 + 1; col2 < cols; col2++) {
                         if (matrix[row1][col1] && matrix[row1][col2] && matrix[row2][col1] && matrix[row2][col2]) {
                             // System.out.println(row1 + "," + row2 + "," + col1 + "," + col2);
