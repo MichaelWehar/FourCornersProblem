bac9564c29174d5b291fb6b1d802170cb76c8655
eevvoor <eevvoor@trash.trash>
2020-09-18 21:19:54 +0200

    stub4find_rectangle.pl

--- perl/find_rectangle.pl
+++ perl/find_rectangle.pl
@@ -43,8 +43,9 @@ print Dumper \@matrix;
 	}
         # Efficiently traverse through pairs of column indexes with 1's (or true entries)
         # First, iterate over all possible entries containing 1 (or true)
-	for (0..$#currentRow-1) { # TODO call last at second last element
-	    my $firstElement = $_;
+	for my $firstIndex (0..$#currentRow-1) { # TODO call last at second last element
+	    my $firstElement = $currentRow[$firstIndex];
+	    print $firstElement;
 	    my $nextElement;
 	    
                 # Encode a pair (firstElement, nextElement) as (firstElement * n) + nextElement
